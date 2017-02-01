a37e4327465b357d2f06aa923dee12131f26167a
Matan Barak <matanb@mellanox.com>
2017-02-01 13:04:02 +0200

    IB/core: Expose ioctl interface through experimental Kconfig
    Add CONFIG_INFINIBAND_EXP_USER_ACCESS that enables the ioctl
    interface. This interface is experimental and is subject to change.
    
    issue: 776663
    Change-Id: I35ecfcae1ffc3dbc82ca29726c1d9204136514e4
    Signed-off-by: Matan Barak <matanb@mellanox.com>
    Reviewed-by: Yishai Hadas <yishaih@mellanox.com>

--- drivers/infiniband/core/uverbs_main.c
+++ drivers/infiniband/core/uverbs_main.c
@@ -940,6 +940,9 @@ static const struct file_operations uverbs_fops = {
 	.open	 = ib_uverbs_open,
 	.release = ib_uverbs_close,
 	.llseek	 = no_llseek,
+#if IS_ENABLED(CONFIG_INFINIBAND_EXP_USER_ACCESS)
+	.unlocked_ioctl = ib_uverbs_ioctl,
+#endif
 };
 
 static const struct file_operations uverbs_mmap_fops = {
@@ -949,6 +952,9 @@ static const struct file_operations uverbs_mmap_fops = {
 	.open	 = ib_uverbs_open,
 	.release = ib_uverbs_close,
 	.llseek	 = no_llseek,
+#if IS_ENABLED(CONFIG_INFINIBAND_EXP_USER_ACCESS)
+	.unlocked_ioctl = ib_uverbs_ioctl,
+#endif
 };
 
 static struct ib_client uverbs_client = {
